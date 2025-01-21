b6bd441bc09355d860041b5e65edc7aba55d0ee2
= <absent.bird@gmail.com>
2025-01-21 14:55:22 -0800

    enabled for demo

--- jsondemo/server.py
+++ jsondemo/server.py
@@ -6,5 +6,6 @@ CORS(app)
 
 @app.route("/")
 def json_frontend():
-    url = request.base_url[:-6]
+    #url = request.base_url[:-6]
+    url = request.base_url
     return render_template("json.html", url=url)
