4c70204dd47c94c822593ddccb0fc2f73d6629fe
Shawn Bird <absent.bird@gmail.com>
2025-02-07 20:35:18 -0800

    added basic admin panel

---
+++ adminpanel/src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-data-table :item-value="ID" :items="items" :headers="headers" show-select return-object>
+  </v-data-table>
+</template>
+
+<script lang="ts" setup>
+import items from "@/assets/hours.json";
+for (const hours of items) {
+  hours.crop = "N/A";
+  hours.worktype = "Other"
+  if (hours.process) {
+    hours.crop = hours.process.harvest.crop.name;
+    hours.worktype = "Processing";
+  } else if (hours.harvest) {
+    hours.crop = hours.harvest.crop.name;
+    hours.worktype = "Harvesting"
+  }
+}
+const headers = [
+  { title: 'Crop', key: 'crop'},
+  { title: 'Work Type', key: 'worktype'},
+  { title: 'Name', key: 'worker.name' },
+  { title: 'Start', key: 'start' },
+  { title: 'Hours', key: 'duration' },
+];
+</script>
