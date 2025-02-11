216265667749bcc719e8d369d0acb405c19c8f40
Studbug <nskkid@gmail.com>
2025-02-11 11:34:00 -0800

    Added dashboard wrapper for table

--- adminpanel/src/layouts/default.vue
+++ adminpanel/src/layouts/default.vue
@@ -1,9 +1,98 @@
-<template>
-  <v-main>
-    <router-view />
-  </v-main>
-</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { RouterLink } from "vue-router";
 
-<script lang="ts" setup>
-//
+const drawer = ref(true);
+const route = useRouter();
+
+const items = ref([
+  {
+    title: "Crops",
+    prependIcon: "mdi-view-dashboard-outline",
+    link: true,
+    to: "/",
+  },
+  {
+    title: "Team",
+    prependIcon: "mdi-account-group",
+    link: true,
+    to: "/team",
+  },
+  {
+    title: "Projects",
+    prependIcon: "mdi-briefcase-outline",
+    link: true,
+  },
+  {
+    title: "Calendar",
+    prependIcon: "mdi-calendar",
+    link: true,
+  },
+  {
+    title: "Reports",
+    prependIcon: "mdi-file-chart-outline",
+    link: true,
+  },
+]);
 </script>
+
+<template>
+  <v-layout>
+    <v-navigation-drawer v-model="drawer">
+      <v-list density="compact" item-props :items="items" nav />
+
+      <template #append>
+        <v-list-item
+          class="ma-2"
+          link
+          nav
+          prepend-icon="mdi-cog-outline"
+          title="Settings"
+        />
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar border="b" class="ps-4" flat>
+      <v-app-bar-nav-icon
+        v-if="$vuetify.display.smAndDown"
+        @click="drawer = !drawer"
+      />
+
+      <v-app-bar-title>
+        Organic Farm at The Evergreen State College
+      </v-app-bar-title>
+
+      <template #append>
+        <v-btn class="text-none me-2" height="48" icon slim>
+          <v-avatar color="surface-light" size="32" />
+
+          <v-menu activator="parent">
+            <v-list density="compact" nav>
+              <v-list-item
+                append-icon="mdi-cog-outline"
+                link
+                title="Settings"
+              />
+
+              <v-list-item append-icon="mdi-logout" link title="Logout" />
+            </v-list>
+          </v-menu>
+        </v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <div class="pa-4">
+        <v-sheet
+          border="dashed md"
+          color="surface-light"
+          rounded="lg"
+          width="100%"
+        >
+          <router-view />
+        </v-sheet>
+      </div>
+    </v-main>
+  </v-layout>
+</template>
---
+++ adminpanel/src/pages/team.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="v-text-h1" style="text-align: center; padding: 20px">
+    Student Hours
+  </h1>
+  <v-data-table
+    style="max-width: 90%; margin: auto; margin-bottom: 33px"
+    :item-value="ID"
+    :items="items"
+    :headers="headers"
+    show-select
+    return-object
+  />
+</template>
+
+<script setup lang="ts">
+import items from "@/assets/hours.json";
+
+const headers = [
+  { title: "Name", key: "worker.name" },
+  { title: "Hours", key: "duration" },
+];
+</script>
