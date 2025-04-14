f80eea3320efb04c9500aa2b7e2791055ba9b7e0
Shawn Bird <absent.bird@gmail.com>
2025-04-14 16:50:48 -0700

    updated script to accept harvest tasks

--- scripts/add-tasks.py
+++ scripts/add-tasks.py
@@ -1,14 +1,25 @@
 import csv
+import os
 import requests
 import sys
 
 csvinput = "tasks.csv"
+env_var = "FARMAPITOKEN"
 apiurl = "https://api.tesc.farm"
 #apiurl = "http://127.0.0.1:8078"
 
 headers = {"Content-Type": "application/json"}
+if os.environ[env_var]:
+    headers['Authorization'] = os.environ[env_var]
 if len(sys.argv) > 1:
-    headers['Authorization'] = sys.argv[1]
+    csvinput = sys.argv[1]
+
+crop_response = requests.get(apiurl + "/crops")
+if crop_response.status_code != 200:
+    print("Error getting crops.")
+    quit()
+crop_array = crop_response.json()
+crops = {crop['name']+crop['variety']: crop for crop in crop_array}
 
 with open(csvinput, 'r') as file:
     csv_reader = csv.reader(file)
@@ -19,22 +30,25 @@ with open(csvinput, 'r') as file:
         payload['tags'] = []
         if task[1] == 'preharvest':
             payload['tags'].append({'name': 'Preharvest'})
+            planting = {}
             newcrop = {'name': task[0]}
             parts = task[0].split(", ")
-            if len(parts) == 2:
-                payload['name'] = f"{parts[1]} {parts[0]}"
-                newcrop['name'] = parts[0]
-                newcrop['variety'] = parts[1]
-            newcrop['tags'] = [{'name': parts[0]}]
-            planting = {}
-            response = requests.post(apiurl + "/crop/new", headers=headers, json=newcrop)
-            if response.status_code == 200:
-                data = response.json()
-                print("Response:", data)
-                planting['crop_id'] = data['ID']
+            if ''.join(parts) in crops:
+                planting['crop_id'] = crops[''.join(parts)]['ID']
             else:
-                print("Error:", response.text)
-                quit()
+                if len(parts) == 2:
+                    payload['name'] = f"{parts[1]} {parts[0]}"
+                    newcrop['name'] = parts[0]
+                    newcrop['variety'] = parts[1]
+                newcrop['tags'] = [{'name': parts[0]}]
+                response = requests.post(apiurl + "/crop/new", headers=headers, json=newcrop)
+                if response.status_code == 200:
+                    data = response.json()
+                    print("Response:", data)
+                    planting['crop_id'] = data['ID']
+                else:
+                    print("Error:", response.text)
+                    quit()
             response = requests.post(apiurl + "/planting/new", headers=headers, json=planting)
             if response.status_code == 200:
                 data = response.json()
@@ -44,6 +58,36 @@ with open(csvinput, 'r') as file:
                 print("Error:", response.text)
                 quit()
             payload['description'] = "Preharvest " + payload['name']
+        elif task[1] == 'harvest':
+            payload['tags'].append({'name': 'Harvest'})
+            harvest = {}
+            newcrop = {'name': task[0]}
+            parts = task[0].split(", ")
+            if ''.join(parts) in crops:
+                harvest['crop_id'] = crops[''.join(parts)]['ID']
+            else:
+                if len(parts) == 2:
+                    payload['name'] = f"{parts[1]} {parts[0]}"
+                    newcrop['name'] = parts[0]
+                    newcrop['variety'] = parts[1]
+                newcrop['tags'] = [{'name': parts[0]}]
+                response = requests.post(apiurl + "/crop/new", headers=headers, json=newcrop)
+                if response.status_code == 200:
+                    data = response.json()
+                    print("Response:", data)
+                    harvest['crop_id'] = data['ID']
+                else:
+                    print("Error:", response.text)
+                    quit()
+            response = requests.post(apiurl + "/harvest/new", headers=headers, json=harvest)
+            if response.status_code == 200:
+                data = response.json()
+                print("Response:", data)
+                payload['harvest_id'] = data['ID']
+            else:
+                print("Error:", response.text)
+                quit()
+            payload['description'] = "Harvest " + payload['name']
         elif task[1] == "mu":
             payload['tags'].append({'name': 'Management Unit'})
             payload['description'] = 'Work in ' + payload['name']
