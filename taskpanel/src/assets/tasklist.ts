21b04d91b39bc346e6aa7addeea31c434fb578f8
Shawn Bird <absent.bird@gmail.com>
2025-04-28 17:57:21 -0700

    moved mu7

--- taskpanel/src/assets/tasklist.ts
+++ taskpanel/src/assets/tasklist.ts
@@ -1,3 +1,3 @@
-const focusFilter: Array<number> = [60, 61, 62, 63, 64, 65, 66, 23, 31, 33, 39, 59, 48, 49, 68, 67, 50, 57]
+const focusFilter: Array<number> = [60, 61, 62, 63, 64, 65, 66, 23, 31, 33, 39, 59, 48, 49, 68, 50, 67, 57]
 
 export default focusFilter
