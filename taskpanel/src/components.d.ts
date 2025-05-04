b2b80a3ddc7fdaa07d0ae9a221522b252ae12752
millimelon <aco2@evergreen.edu>
2025-05-04 12:56:53 -0700

    Created TaskCard, finalized accessibility nav.

--- taskpanel/src/components.d.ts
+++ taskpanel/src/components.d.ts
@@ -11,5 +11,6 @@ declare module 'vue' {
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TaskCard: typeof import('./components/TaskCard.vue')['default']
   }
 }
