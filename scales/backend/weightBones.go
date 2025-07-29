58919569e03800d2faf6ce258a5e548b6d3a31d1
Amy Olson <aco2@evergreen.edu>
2025-07-29 12:41:28 -0700

    Adding structure and fixing Bones before testing front end connections

--- scales/backend/weightBones.go
+++ scales/backend/weightBones.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type WeighIn struct {
-
-	ProduceID		int		'json:"produce_id"'
-	GrossWeight		float64		'json:"gross_weight"'
-	ToMarketWeight		float64		'json:"to_market_weight"'
-	SoldToStudents		float64		'json:"sold_to_students"'
-	ValueAdded		float64		'json:"value_added"'
-	Cull		float64		'json:"cull"'
-	Timestamp		time.Time		'json:"timestamp"'
+	ProduceID      int       `json:"produce_id"`
+	GrossWeight    float64   `json:"gross_weight"`
+	ToMarketWeight float64   `json:"to_market_weight"`
+	SoldToStudents float64   `json:"sold_to_students"`
+	ValueAdded     float64   `json:"value_added"`
+	Cull           float64   `json:"cull"`
+	Timestamp      time.Time `json:"timestamp"`
 }
 
 func main() {
 	r := gin.Default()
+
+	var storedWeighIns []WeighIn // testing
 
 	r.POST("/weigh-in", func(c *gin.Context) {
 		var w WeighIn
@@ -27,15 +29,14 @@ func main() {
 			return
 		}
 
-		// insert into DB here
+		storedWeighIns = append(storedWeighIns, w)
 
 		c.JSON(http.StatusOK, gin.H{"status": "data received", "data": w})
 	})
+
+	r.GET("/weigh-ins", func(c *gin.Context) {
+		c.JSON(http.StatusOK, storedWeighIns)
+	})
+
 	r.Run(":8080") //listen and serve on 0.0.0.0:8080
 }
-
-var storedWeighIns []WeighIn // testing
-
-r.GET("/weigh-ins", func(c *gin.Context) {
-    c.JSON(http.StatusOK, storedWeighIns)
-})
